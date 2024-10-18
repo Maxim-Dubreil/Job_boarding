f744983d7625dbc6d3c115cfaeeb4b1aaf2e169d
Abdou Aziz Daback Ba <150848147+Aking221@users.noreply.github.com>
2024-10-18 09:15:46 +0200

    search bar

--- jobboard_frontend/src/components/SearchBar.jsx
+++ jobboard_frontend/src/components/SearchBar.jsx
@@ -1,17 +1,47 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/SearchBar.css';
+import SearchIcon from '@mui/icons-material/Search';
 
-function SearchBar () {
-    return(
-        <div className='search-bar-container'>
-            <div>
-                SearchBar
-            </div>
-            <div>
-                SearchResults
-            </div>
+function SearchBar() {
+  const [what, setWhat] = useState('');
+  const [where, setWhere] = useState('');
+  const navigate = useNavigate();
 
+  const handleSearch = () => {
+    // Redirect to a search result page or filter job offers in Home based on the search query
+    navigate('/', { state: { what, where } });
+  };
+
+  return (
+    <div className="search-bar">
+      <div className="input-wrapper">
+        <SearchIcon className="search-icon" />
+        <div className="search-fields">
+          <div className="search-what">
+            <input
+              type="text"
+              placeholder="What? ex: Profession ? sector of activity ?"
+              value={what}
+              onChange={(e) => setWhat(e.target.value)}
+            />
+          </div>
+          <div className="separator">|</div>
+          <div className="search-where">
+            <input
+              type="text"
+              placeholder="Where? ex: Rennes, Paris"
+              value={where}
+              onChange={(e) => setWhere(e.target.value)}
+            />
+          </div>
         </div>
+      </div>
+      <button className="search-button" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
+}
 
-)}
-
-export default SearchBar
+export default SearchBar;
