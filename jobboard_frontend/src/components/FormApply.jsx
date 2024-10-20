3bf001cf1d6854e17e3a507d6eebe5b5fea49e6d
Maxim <m.dubreil25@gmail.com>
2024-10-20 15:44:38 +0200

    fix

---
+++ jobboard_frontend/src/components/FormApply.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+import '../styles/advert.css';
+import '../styles/advertright.css'
+
+function FormApply (){
+
+}
+
+export default FormApply;
